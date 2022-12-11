d27ed64c287b65b2e172ae5f58fc17b620be4c08
Megan Yang <meimeipuppy@gmail.com>
2022-12-11 15:05:22 +0800

    feature: change the theme color and layout of main content

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -1,28 +1,14 @@
 <template>
-  <header
-    id="header"
-    class="header bg-purple-900 flex flex-col md:flex-row md:justify-between items-center p-4 md:p-6"
-    role="banner"
-  >
-    <g-link
-      class="company-logo-link block text-white font-bold text-2xl md:text-3xl hover:text-pink-500"
-      to="/"
-      aria-label="Back to home"
-    >{{ $static.metadata.siteName }}</g-link>
+  <header id="header" class="flex flex-col items-center p-4 header bg-purple-650 md:flex-row md:justify-between md:p-6"
+    role="banner">
+    <g-link class="block text-2xl font-bold text-white company-logo-link md:text-3xl hover:text-yellow-350" to="/"
+      aria-label="Back to home">{{ $static.metadata.siteName }}</g-link>
 
-    <nav id="nav" class="nav hidden md:flex">
-      <ul class="menu flex flex-col md:flex-row items-center list-reset text-base">
-        <li
-          class="level-1 mb-4 md:mr-4 md:mb-0"
-          v-for="element in $static.metadata.menu"
-          :key="element.name"
-        >
-          <g-link
-            :to="element.link"
-            class="link font-bold text-white hover:text-pink-500"
-            active-class="is-active-link"
-            exact-active-class="active text-pink-500"
-          >{{ element.name }}</g-link>
+    <nav id="nav" class="hidden nav md:flex">
+      <ul class="flex flex-col items-center text-base menu md:flex-row list-reset">
+        <li class="mb-4 level-1 md:mr-4 md:mb-0" v-for="element in $static.metadata.menu" :key="element.name">
+          <g-link :to="element.link" class="font-bold text-white link hover:text-yellow-350"
+            active-class="is-active-link" exact-active-class="text-purple-650 active">{{ element.name }}</g-link>
         </li>
       </ul>
     </nav>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,27 @@
 <template>
   <Layout>
-    <section id="container-centre" class="column centre flex-1">
-      <h1 class="page-title">Welcome to Skeleventy!</h1>
-      <div class="content">
+    <section id="container-centre" class="flex flex-wrap-reverse centre">
+
+      <div class="justify-center content">
+        <h1 class="page-title"> My Reward Kit</h1>
         <p>
-          A 
+          A
           <a href="https://www.gridsome.org">Gridsome</a> starter built with
           <a href="https://tailwindcss.com">TailwindCSS</a>.
         </p>
         <h2 class="text-3xl">Features</h2>
-        <ul class="mb-12 list-disc pl-4">
+        <ul class="pl-4 mb-12 list-disc">
           <li>Build sites faster with the power of Gridsome, TailwindCSS and SCSS</li>
           <li>Image lazyloading via gridsome builtin functionality</li>
           <li>A simple blog with categories, tags and featured images</li>
         </ul>
         <h2 class="text-3xl">Credits</h2>
         <p>
-          This template was built for <a href="https://www.11ty.dev/docs/" target="_blank">Eleventy</a> by <a href="https://github.com/josephdyer" target="_blank"> Joseph Dyer</a>.
+          This template was built for <a href="https://www.11ty.dev/docs/" target="_blank">Eleventy</a> by <a
+            href="https://github.com/josephdyer" target="_blank"> Joseph Dyer</a>.
         </p>
       </div>
+      <g-image src="~/myreward.png" width="500"></g-image>
     </section>
   </Layout>
 </template>
