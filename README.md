d09c1932a44f0e428ad4dcdd5adce1a775884deb
Megan Yang <meimeipuppy@gmail.com>
2022-12-11 14:33:05 +0800

    init: gridsome skeleventy starter

---
+++ README.md
@@ -0,0 +1,36 @@
+<div align="center">
+
+<a href="https://www.jammeryhq.com" title="JammeryHQ" target="_blank">
+
+  <img src="https://jammeryhq.com/jammeryhq.png" width="128" />
+  
+</a>
+
+<p>
+Fast-track your JAMstack development & learning
+</p>
+</div>
+
+<hr />
+
+# About this starter
+
+Gridsome starter based on the Skeleventy theme for Eleventy. Content is added via markdown, while Tailwind CSS is used for the layout/styling.
+
+## Key features
+
+* Built with Tailwind CSS
+* Responsive 
+* Pagination
+
+## Installation
+
+Run the following command to create a new gridsome project 
+
+```
+gridsome create my-blog https://github.com/jammeryhq/gridsome-starter-skeleventy
+```
+
+## Credits
+
+This starter is based on the theme from [Joseph Dyer](https://github.com/josephdyer).
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <section id="container-centre" class="column centre flex-1">
+      <h1 class="page-title">Welcome to Skeleventy!</h1>
+      <div class="content">
+        <p>
+          A 
+          <a href="https://www.gridsome.org">Gridsome</a> starter built with
+          <a href="https://tailwindcss.com">TailwindCSS</a>.
+        </p>
+        <h2 class="text-3xl">Features</h2>
+        <ul class="mb-12 list-disc pl-4">
+          <li>Build sites faster with the power of Gridsome, TailwindCSS and SCSS</li>
+          <li>Image lazyloading via gridsome builtin functionality</li>
+          <li>A simple blog with categories, tags and featured images</li>
+        </ul>
+        <h2 class="text-3xl">Credits</h2>
+        <p>
+          This template was built for <a href="https://www.11ty.dev/docs/" target="_blank">Eleventy</a> by <a href="https://github.com/josephdyer" target="_blank"> Joseph Dyer</a>.
+        </p>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Home"
+  }
+};
+</script>
